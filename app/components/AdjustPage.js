f38b38ea684e74dcc9bfef3791bfc198fe6b296d
Charles Wesley Cho <charleswcho@gmail.com>
2016-09-11 15:17:00 -0700

    Fixing Adjust Page render bug

--- app/components/AdjustPage.js
+++ app/components/AdjustPage.js
@@ -23,48 +23,50 @@ export default class AdjustPage extends Component {
     ResultStore.removeChangeListener(this._resultsChanged);
   }
 
-  parseDiff() {
-    let diff = {},
-        desiredPort = this.state.desiredPortfolio,
-        actualPort = this.state.actualPortfolio,
-        actualSum = this.state.actualSum
-
-    console.log('actual port', actualPort, 'desiredPort', desiredPort)
-
-    desiredPort.forEach((asset, idx) => {
-      // Calculate the target value and find the difference and percent difference with the actual value of the user's asset
-
-      let desiredVal = (asset.y / 100) * actualSum,
-          actualVal = actualPort[idx].y,
-          percentDelta = ((desiredVal - actualVal) / actualVal) * 100;
-
-      diff[asset.x] = [desiredVal - actualVal, percentDelta]
-    })
-
-    // Format of diff object { assetName: [diff, percent diff] }
-    return diff
-  }
-
-  renderDiff() {
-    let diff = this.parseDiff();
-
-    return (
-        {
-          Object.keys(diff).map((asset, idx) => {
-            return <View key={idx}><Text>{asset + ' '}</Text>
-              {this.formatAssetVal(asset, diff[asset])}</View>
-          })
-        }
-    )
-  }
-
-  formatAssetVal(asset, val) {
-    return (
-      <Text className={(val[0] < 0) ? 'negative' : 'positive'}>
-         {val[1].toFixed(2) + '% | $ ' + val[0].toFixed(2)}
-      </Text>
-    )
-  }
+  // parseDiff() {
+  //   let diff = {},
+  //       desiredPort = this.state.desiredPortfolio,
+  //       actualPort = this.state.actualPortfolio,
+  //       actualSum = this.state.actualSum
+  //
+  //   console.log('actual port', actualPort, 'desiredPort', desiredPort)
+  //
+  //   desiredPort.forEach((asset, idx) => {
+  //     // Calculate the target value and find the difference and percent difference with the actual value of the user's asset
+  //
+  //     let desiredVal = (asset.y / 100) * actualSum,
+  //         actualVal = actualPort[idx].y,
+  //         percentDelta = ((desiredVal - actualVal) / actualVal) * 100;
+  //
+  //     diff[asset.x] = [desiredVal - actualVal, percentDelta]
+  //   })
+  //
+  //   // Format of diff object { assetName: [diff, percent diff] }
+  //   return diff
+  // }
+  //
+  // renderDiff() {
+  //   let diff = this.parseDiff();
+  //
+  //   return (
+  //     {Object.keys({ hi: 1, there: 2}).map((asset, idx) => {
+  //       return (
+  //         <View key={idx}>
+  //           <Text>{asset + ' '}</Text>
+  //           <View>{this.formatAssetVal(diff[asset])}</View>
+  //         </View>
+  //       )
+  //     })}
+  //   )
+  // }
+  //
+  // formatAssetVal(val)
+  //   return (
+  //     <Text className={(val[0] < 0) ? 'negative' : 'positive'}>
+  //        {val[1].toFixed(2) + '% | $ ' + val[0].toFixed(2)}
+  //     </Text>
+  //   )
+  // }
 
   _resultsChanged = () => {
     this.setState({
@@ -82,8 +84,7 @@ export default class AdjustPage extends Component {
         <Text>Current Portfolio</Text>
         <DonutChart data={this.state.actualPortfolio}/>
 
-
-        <Text>Target Portfolio</text>
+        <Text>Target Portfolio</Text>
         <DonutChart data={this.state.desiredPortfolio}/>
       </View>
     );
